3efd7ff006de4056e4d7782c9a8a20ceb1be554e
Aitor <zheladev@gmail.com>
2020-12-26 14:52:45 +0100

    Add signup component, use vuelidate package

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import Vuelidate from 'vuelidate';
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   router,
