452cb360cea3842ccd02ddf284ea8dcf7e423123
Aitor <zheladev@gmail.com>
2021-01-10 19:10:08 +0100

    Refactor parallax into separate component

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,45 +1,71 @@
 <template>
-  <v-container pa-0>
-    <landing-carousel />
-    <boat-grid :boats="allBoats" />
-    <v-pagination v-if="allBoats.length>0"
+  <v-container fluid class="pa-0">
+    <parallax-image>
+      <v-col class="text-center" cols="12">
+        <v-col>
+            <v-form dark>
+          <v-row class="pa-2">
+            <v-col class="blue">
+              <v-text-field dark placerholder="hello"> </v-text-field>
+            </v-col>
+            <v-col class="red">
+              <v-text-field dark> </v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+        </v-col>
+      </v-col>
+    </parallax-image>
+    <v-container pa-0>
+      <boat-grid :boats="allBoats" />
+      <v-pagination
+        v-if="allBoats.length > 0"
         v-model="page"
         :length="pages"
-        @input="updateBoats"></v-pagination>
+        @input="updateBoats"
+      ></v-pagination>
+    </v-container>
   </v-container>
 </template>
 
 <script>
-import BoatGrid from './BoatGrid.vue';
-import LandingCarousel from './LandingCarousel.vue';
+import BoatGrid from "./BoatGrid.vue";
 import { mapActions, mapGetters } from "vuex";
+import ParallaxImage from "./common/ParallaxImage.vue";
 
 export default {
-  components: { LandingCarousel, BoatGrid },
+  components: { BoatGrid, ParallaxImage },
   name: "Landing",
   data: () => ({
-      page: 1,
-      limit: 15,
+    page: 1,
+    limit: 15,
+    port: '',
+    passengerCapacity: '',
+    numberOfBathrooms: '',
+    numberOfCabins: '',
+    date: '', // not implemented
+
+
   }),
   computed: {
-      ...mapGetters(["allBoats", "maxPages"]),
-      pages() {
-          return Math.ceil(this.maxPages);
-      }
+    ...mapGetters(["allBoats", "maxPages"]),
+    pages() {
+      return Math.ceil(this.maxPages);
+    },
   },
   methods: {
     ...mapActions(["fetchBoats"]),
     async updateBoats() {
-        await this.fetchBoats({
-            page: this.page-1,
-            limit: this.limit,
-        })
-    }
+      await this.fetchBoats({
+        page: this.page - 1,
+        limit: this.limit,
+      });
+    },
   },
   async mounted() {
     await this.fetchBoats({
-        page: this.page-1,
-        limit: this.limit,
+      page: this.page - 1,
+      limit: this.limit,
     });
   },
 };
---
+++ src/components/common/ParallaxImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-parallax
+      dark
+      src="https://images.reference.com/amg-cms-reference-images/media/oceans-seas-surround-africa_ac189a10d9c855c0.jpg"
+    >
+      <v-row align="center" justify="center" class="parallax">
+        <slot></slot>
+      </v-row>
+    </v-parallax>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.parallax {
+    background-color: rgba(0, 0, 0, 0.281);
+}
+.no-padding {
+  padding: 0px;
+}
+.parallax-text {
+  padding-left: 0px;
+  padding-right: 0px;
+}
+</style>
+
+<style>
+.v-parallax__content {
+    padding: 0px 0px !important;
+}
+</style>
