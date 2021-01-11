06716e2b2ae2206e3419c488aebf3cdfaef3dc18
Aitor <zheladev@gmail.com>
2021-01-11 19:41:51 +0100

    Add search dropdown for smaller displays

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -9,13 +9,16 @@
         <h4 class="subheading">Building bridges beween people.</h4>
       </v-col>
     </parallax-image>
-      <v-sheet class="blue lighten-5" :class="smAndDown ? '' : 'sticky'" elevation="1">
-        <div class="text-center headline pt-4">
-          Find the boat of your dreams
-        </div>
-        <v-form>
-          <v-container>
-            <v-row>
+    <v-sheet
+      v-if="!mdAndDown"
+      class="blue lighten-5"
+      :class="mdAndDown ? '' : 'sticky'"
+      elevation="1"
+    >
+      <div class="text-center headline pt-4">Find the boat of your dreams</div>
+      <v-form>
+        <v-container>
+          <v-row>
             <v-col cols="12" md="6" lg="3">
               <v-sheet class="blue lighten-5">
                 <v-row no-gutters>
@@ -47,7 +50,7 @@
                         <template v-slot:activator="{ on }">
                           <v-row no-gutters>
                             <v-text-field
-                            class="pl-2 pr-4"
+                              class="pl-2 pr-4"
                               label="Rental duration"
                               prepend-icon="mdi-calendar"
                               readonly
@@ -80,7 +83,7 @@
                       :items="filterTypes"
                     ></v-select>
                   </v-col>
-                  <v-col cols="8" sm="10" md="9" lg="7" >
+                  <v-col cols="8" sm="10" md="9" lg="7">
                     <v-text-field
                       class="pr-4"
                       v-model="price"
@@ -95,9 +98,7 @@
             <v-col cols="12" md="6" lg="3">
               <v-sheet class="blue lighten-5">
                 <v-row>
-                  <v-col cols="2" md="0">
-
-                  </v-col>
+                  <v-col cols="2" md="0"> </v-col>
                   <v-col cols="7" class="">
                     <v-btn
                       class="green lighten-1"
@@ -116,16 +117,143 @@
                       ><v-icon>delete</v-icon></v-btn
                     >
                   </v-col>
-                  <v-col cols="1">
-
-                  </v-col>
+                  <v-col cols="1"> </v-col>
                 </v-row>
               </v-sheet>
             </v-col>
           </v-row>
-          </v-container>
-        </v-form>
-      </v-sheet>
+        </v-container>
+      </v-form>
+    </v-sheet>
+    <v-expansion-panels
+      v-bind="smAndDown"
+      v-model="mobileSearchPanel"
+      v-else
+      class="sticky"
+      :style="searchDropdownStyle"
+      focusable
+    >
+      <v-expansion-panel>
+        <v-expansion-panel-header class="blue lighten-4" :ripple="true">
+          <div>Search {{ mobileSearchPanel }}</div>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content class="blue lighten-5">
+          <v-sheet class="blue lighten-5">
+            <div class="text-center headline pt-4">
+              Find the boat of your dreams
+            </div>
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="6" lg="3">
+                    <v-sheet class="blue lighten-5">
+                      <v-row no-gutters>
+                        <v-col>
+                          <v-select
+                            class="pr-4 pl-2"
+                            label="Port"
+                            prepend-icon="location_on"
+                            v-model="port"
+                            :items="availablePorts"
+                          ></v-select>
+                        </v-col>
+                      </v-row>
+                    </v-sheet>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="3">
+                    <v-sheet>
+                      <v-row no-gutters>
+                        <v-col>
+                          <v-sheet class="blue lighten-5 sticky transparent">
+                            <v-menu
+                              ref="dateMenu"
+                              v-model="dateMenu"
+                              :close-on-content-click="false"
+                              transition="scale-transition"
+                              offset-y
+                              min-width="290px"
+                            >
+                              <template v-slot:activator="{ on }">
+                                <v-row no-gutters>
+                                  <v-text-field
+                                    class="pl-2 pr-4"
+                                    label="Rental duration"
+                                    prepend-icon="mdi-calendar"
+                                    readonly
+                                    :value="formattedDate"
+                                    v-on="on"
+                                  ></v-text-field>
+                                </v-row>
+                              </template>
+                              <v-date-picker
+                                v-model="dates"
+                                @input="formatDates"
+                                range
+                              >
+                              </v-date-picker>
+                            </v-menu>
+                          </v-sheet>
+                        </v-col>
+                      </v-row>
+                    </v-sheet>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="3">
+                    <v-sheet class="blue lighten-5">
+                      <v-row no-gutters>
+                        <v-col cols="4" sm="2" md="3" lg="5">
+                          <v-select
+                            class="pl-2 text-truncate"
+                            label="Filter"
+                            prepend-icon="euro"
+                            v-model="filter"
+                            :items="filterTypes"
+                          ></v-select>
+                        </v-col>
+                        <v-col cols="8" sm="10" md="9" lg="7">
+                          <v-text-field
+                            class="pr-4"
+                            v-model="price"
+                            label="Price per day"
+                            type="number"
+                          >
+                          </v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-sheet>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="3">
+                    <v-sheet class="blue lighten-5">
+                      <v-row>
+                        <v-col cols="2" md="0"> </v-col>
+                        <v-col cols="7" class="">
+                          <v-btn
+                            class="green lighten-1"
+                            block
+                            max-width="170"
+                            @click="searchBoats"
+                            >Find</v-btn
+                          >
+                        </v-col>
+                        <v-col cols="2">
+                          <v-btn
+                            :class="isFiltered ? 'red--text' : ''"
+                            :disabled="!isFiltered"
+                            icon
+                            @click="deleteFilter"
+                            ><v-icon>delete</v-icon></v-btn
+                          >
+                        </v-col>
+                        <v-col cols="1"> </v-col>
+                      </v-row>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-sheet>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
     <v-container pa-0>
       <boat-grid :boats="allBoats" />
       <v-pagination
@@ -152,8 +280,9 @@ export default {
       new Date().toISOString().split("T")[0],
       new Date().toISOString().split("T")[0],
     ],
+    mobileSearchPanel: undefined,
     formattedDate: "",
-    filter: '',
+    filter: "",
     price: null,
     dateMenu: false,
     page: 1,
@@ -161,7 +290,7 @@ export default {
     port: null,
     filterTypes: [
       {
-        text: 'None',
+        text: "None",
         value: null,
       },
       {
@@ -190,11 +319,18 @@ export default {
   }),
   computed: {
     ...mapGetters(["allBoats", "maxPages", "allPorts"]),
+    searchDropdownStyle() {
+      const mdAndUpTop = "64px";
+      const smAndDownTop = "56px";
+      return {
+        top: this.$vuetify.breakpoint.smAndDown ? smAndDownTop : mdAndUpTop,
+      };
+    },
     pages() {
       return Math.ceil(this.maxPages);
     },
-    smAndDown() {
-      return this.$vuetify.breakpoint.smAndDown;
+    mdAndDown() {
+      return this.$vuetify.breakpoint.mdAndDown;
     },
     availablePorts() {
       return this.allPorts.map((p) => ({
@@ -220,7 +356,8 @@ export default {
       }
 
       if (price !== null) {
-        const filterOp = this.filter === null || this.filter === '' ? ":" : this.filter;
+        const filterOp =
+          this.filter === null || this.filter === "" ? ":" : this.filter;
         searchParams = searchParams.concat(`pricePerDay${filterOp}${price},`);
       }
 
@@ -238,6 +375,7 @@ export default {
 
       this.page = 1;
       this.isFiltered = false;
+      this.mobileSearchPanel = undefined;
     },
     async searchBoats() {
       await this.fetchBoats({
@@ -248,6 +386,7 @@ export default {
 
       this.page = 1;
       this.isFiltered = true;
+      this.mobileSearchPanel = undefined;
     },
     async updateBoats() {
       await this.fetchBoats({
@@ -288,8 +427,8 @@ export default {
 
 <style>
 .sticky {
-   position: sticky;
-   top: 0;
-   z-index: 2;
+  position: sticky;
+  top: 0;
+  z-index: 2;
 }
 </style>
