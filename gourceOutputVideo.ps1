4793e9baf83b9bb16be82d20471c2fc106fea9b4
Aitor <zheladev@gmail.com>
2021-01-12 22:45:04 +0100

    update repo video out script

--- gourceOutputVideo.ps1
+++ gourceOutputVideo.ps1
@@ -1,2 +1,2 @@
-gource -s .06 -1280x720 --auto-skip-seconds .1 --multi-sampling --stop-at-end --key --highlight-users --hide mouse,progress,files,filenames,dirnames --file-idle-time 0     --max-files 0      --background-colour 000000     --font-size 22     --title "Frontend" --output-ppm-stream output.ppm  --output-framerate 30 
+gource -s .06 -1280x720 --auto-skip-seconds .1 --multi-sampling --stop-at-end --key --hide mouse,progress,users,date --file-idle-time 0     --max-files 0      --background-colour 000000     --font-size 22     --title "Backend" --output-ppm-stream output.ppm  --output-framerate 30 
 ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i output.ppm  -vcodec wmv1 -r 60 -qscale 0 out.wmv
